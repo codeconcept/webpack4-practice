1ed5826f368dad047578b6e4b79835199165c8b0
codeconcept <contact@codeconcept.fr>
2018-11-27 18:07:14 +0100

    customizing progress report by passing arguments to the plugin constructor

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,19 @@
 const path =  require('path');
 const webpack = require('webpack');
 
+const handler = (percentage, message, ...args) => {
+  const percent = (percentage * 100).toFixed(2); 
+  const msg = message.toUpperCase();
+  const argsOrDefault = (args.length === 0) ? "" : args.join(' | ');
+  console.info(`${percent}%`, msg, argsOrDefault);
+};
+
+// // from the official doc https://webpack.js.org/plugins/progress-plugin/
+// const handler = (percentage, message, ...args) => {
+//   // e.g. Output each progress message directly to the console:
+//   console.info(percentage, message, ...args);
+// };
+
 module.exports = {
   entry: './src/index.js',
   output: {
@@ -21,6 +34,6 @@ module.exports = {
     ]
   },
   plugins: [
-    new webpack.ProgressPlugin()
+    new webpack.ProgressPlugin(handler)
   ]
 }
